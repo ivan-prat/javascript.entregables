e56b8d7920e64b78858f0fcbdde51ccf03b47d8e
ivan-prat <ivanpratauzmendi@gmail.com>
2026-09-06 17:32:29 -0300

    Entregable Clase 1

---
+++ main.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingrese su nombre:");
+alert("Hola, " + nombre + "! Bienvenido a mi sitio web.");
+
+let nacimiento = parseInt(prompt("Ingrese año de nacimiento:"));
+alert("Tienes " + (2026 - nacimiento) + " años.");
+
+let club = prompt("¿Cual es su club de futbol favorito?:");
+alert("¡Usted ha seleccionado " + club + "!" + " Excelente elección!");
