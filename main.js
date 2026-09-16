d1b1cba125d070435963127f4f85da2bf90b5e97
ivan-prat <ivanpratauzmendi@gmail.com>
2026-09-15 22:11:31 -0300

    Entregable 3

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
+function solicitarRol() {
+    let entrada = prompt("Te gustaria ingresar como Usuario, Administrador o Invitado?:")?.toLowerCase();
+    return entrada;
+}
+function mostrarMensaje(rolIngresado) {
+    if (rolIngresado === "administrador") {
+    return("Bienvenido, Acceso completo al sistema\nTe gustaria volver a ingresar como otro rol?");
+} else if (rolIngresado === "usuario") {
+    return("Bienvenido, Acceso a tu panel de usuario\nTe gustaria volver a ingresar como otro rol?");
+} else if (rolIngresado === "invitado") {
+    return("Bienvenido, Acceso limitado al sistema\nTe gustaria volver a ingresar como otro rol?");
+}
+else {
+    return("Rol no reconocido, ingresa un rol valido");
+}
+}
+const preguntarContinuar = (texto) => confirm(texto);
 let continuar = true;
 let rol;
 do {
 do {
-    rol = prompt("Te gustaria ingresar como Usuario, Administrador o Invitado?:")?.toLowerCase();
+    rol = solicitarRol();
 } while (rol === null || rol === "");
-if (rol === "administrador") {
-    continuar = confirm("Bienvenido, Acceso completo al sistema\nTe gustaria volver a ingresar como otro rol?");
-} else if (rol === "usuario") {
-    continuar = confirm("Bienvenido, Acceso a tu panel de usuario\nTe gustaria volver a ingresar como otro rol?");
-} else if (rol === "invitado") {
-    continuar = confirm("Bienvenido, Acceso limitado al sistema\nTe gustaria volver a ingresar como otro rol?");
-}
-else {
-    alert("Rol no reconocido, ingresa un rol valido");
-}
+let mensaje = mostrarMensaje(rol);
+continuar = preguntarContinuar(mensaje);
 } while (continuar);
