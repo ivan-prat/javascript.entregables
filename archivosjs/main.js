0485b3f1d1bda9d1ab2da4886753727c17a0dcc3
ivan-prat <ivanpratauzmendi@gmail.com>
2026-09-23 14:57:40 -0300

    Entregable 3 corregido

--- archivosjs/main.js
+++ archivosjs/main.js
@@ -1,28 +1,33 @@
-function solicitarRol() {
-    let entrada = prompt("Te gustaria ingresar como Usuario, Administrador o Invitado?:")?.toLowerCase();
-    return entrada;
+let tipoDeUsuario = prompt("Le gustaria ingresar como cliente o como proveedor?");
+let repuestos = ["filtro de aceite", "bujias", "pastillas de freno", "amortiguadores", "correa de distribucion", "bateria", "radiador", "faro delantero", "espejo retrovisor", "paragolpes", "ventilador", "embrague", "alternador", "catalizador", "manguera de radiador", "tubo de escape", "sensor de oxigeno", "filtro de aire", "filtro de combustible", "bomba de agua", "bomba de combustible", "caja de cambios", "disco de freno", "pastilla de embrague", "amortiguador trasero", "amortiguador delantero", "faro trasero", "espejo lateral", "parabrisas", "limpiaparabrisas", "bomba de direccion asistida", "bomba de freno", "cilindro maestro de freno", "cilindro esclavo de embrague", "cable de acelerador"];
+const mostrarMensaje = (mensaje) => {
+    alert(mensaje);
 }
-function mostrarMensaje(rolIngresado) {
-    if (rolIngresado === "administrador") {
-    return("Bienvenido, Acceso completo al sistema\nTe gustaria volver a ingresar como otro rol?");
-} else if (rolIngresado === "usuario") {
-    return("Bienvenido, Acceso a tu panel de usuario\nTe gustaria volver a ingresar como otro rol?");
-} else if (rolIngresado === "invitado") {
-    return("Bienvenido, Acceso limitado al sistema\nTe gustaria volver a ingresar como otro rol?");
+function existeEnStock(producto, lista) {
+    return lista.includes(producto);
 }
-else {
-    return("Rol no reconocido, ingresa un rol valido");
+function agregarRepuesto(producto, lista) {
+    lista.push(producto);
+}
+if (tipoDeUsuario === "proveedor") {
+    let nombreProveedor = prompt("Ingrese su nombre completo");
+    mostrarMensaje("Bienvenido " + nombreProveedor);
+let repuestoProveedor = prompt("Ingrese el repuesto que desea vender");
+if (existeEnStock(repuestoProveedor, repuestos)) {
+    mostrarMensaje("El repuesto ya se encuentra en el inventario.");
+} else {
+    agregarRepuesto(repuestoProveedor, repuestos);
+    mostrarMensaje("El repuesto no lo teniamos en stock, ya ha sido agregado, pase por caja a retirar el dinero. ¡Muchas gracias!");
 }
 }
-const preguntarContinuar = (texto) => confirm(texto);
-const despedirUsuario = () => alert("¡Gracias por utilizar mi simulador, hasta luego!");
-let continuar = true;
-let rol;
-do {
-do {
-    rol = solicitarRol();
-} while (rol === null || rol === "");
-let mensaje = mostrarMensaje(rol);
-continuar = preguntarContinuar(mensaje);
-} while (continuar);
-despedirUsuario();
+if (tipoDeUsuario === "cliente") {
+    let nombreCliente = prompt("Ingrese su nombre completo");
+    mostrarMensaje("Bienvenido " + nombreCliente);
+let repuestoSeleccionado = prompt("Ingrese el repuesto que desea comprar");
+if (existeEnStock(repuestoSeleccionado, repuestos)) {
+    mostrarMensaje("El repuesto seleccionado está disponible para la venta.");
+} else {
+    mostrarMensaje("Lo sentimos, el repuesto seleccionado no está disponible para la venta.");
+}
+}
+
